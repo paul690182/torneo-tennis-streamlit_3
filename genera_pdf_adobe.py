c4e63b39f25d81724e867a17b2b84deddcab065f
paul690182 <ric_roland@hotmail.it>
2026-05-03 15:32:58 +0200

    Update genera_pdf_adobe.py

--- genera_pdf_adobe.py
+++ genera_pdf_adobe.py
@@ -22,9 +22,7 @@ df = pd.DataFrame(response.data)
 
 # --- NORMALIZZA COLONNE ---
 df.columns = df.columns.str.strip().str.lower()
-print("COLONNE:", df.columns.tolist())
-print("ANTEPRIMA:")
-print(df.head(10))
+
 
 # --- DEBUG MINIMO (solo per capire) ---
 print("COLONNE REALI:", df.columns.tolist())
